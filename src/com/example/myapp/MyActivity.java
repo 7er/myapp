52118d24665c15f5937e0df60d5ceb8d671fe6c3
Syver Enstad <syvensta@cisco.com>
2014-04-09 10:52:06 +0200

    Change to use list activity

--- src/com/example/myapp/MyActivity.java
+++ src/com/example/myapp/MyActivity.java
@@ -1,24 +1,23 @@
 package com.example.myapp;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.*;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
+import java.util.*;
 
-public class MyActivity extends Activity implements View.OnClickListener {
-    private void fillEventList() {
+public class MyActivity extends ListActivity {
+    private List<String> getEventList() {
         GregorianCalendar beginWindow = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
         beginWindow.setTime(new Date());
         beginWindow.roll(Calendar.DAY_OF_MONTH, false);
@@ -28,7 +27,7 @@ public class MyActivity extends Activity implements View.OnClickListener {
         endWindow.roll(Calendar.DAY_OF_MONTH, true);
 
         String[] projection =
-                new String[]{
+                new String[] {
                         CalendarContract.Instances._ID,
                         CalendarContract.Instances.BEGIN,
                         CalendarContract.Instances.END,
@@ -41,6 +40,7 @@ public class MyActivity extends Activity implements View.OnClickListener {
                 beginWindow.getTimeInMillis(),
                 endWindow.getTimeInMillis());
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy hh:mm");
+        ArrayList<String> result = new ArrayList<String>();
         if (calCursor.moveToFirst()) {
             do {
                 long id = calCursor.getLong(0);
@@ -49,28 +49,28 @@ public class MyActivity extends Activity implements View.OnClickListener {
                 GregorianCalendar end = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
                 end.setTimeInMillis(calCursor.getLong(2));
                 String title = calCursor.getString(4);
-                System.out.println(id + " " + formatter.format(begin.getTime()) + " " + formatter.format(end.getTime()) + " " + title);
+                result.add(id + " " + formatter.format(begin.getTime()) + " " + formatter.format(end.getTime()) + " " + title);
             } while (calCursor.moveToNext());
         }
+        return result;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Button button = (Button) findViewById(R.id.browse);
-        button.setOnClickListener(this);
-        fillEventList();
-        Intent intent = getIntent();
-        if (intent.getAction() == Intent.ACTION_VIEW) {
-            TextView text = (TextView) findViewById(R.id.text);
-            text.setText(intent.getData().toString());
-        }
+        List<String> events = getEventList();
+        setListAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, events));
+//        Intent intent = getIntent();
+//        if (intent.getAction() == Intent.ACTION_VIEW) {
+//            TextView text = (TextView) findViewById(R.id.text);
+//            text.setText(intent.getData().toString());
+//        }
     }
 
 
     @Override
-    public void onClick(View v) {
+    public void onListItemClick(ListView list, View v, int position, long id) {
+        super.onListItemClick(list, v, position, id);
         //new Intent(this, MyActivity.class);
         startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("sip:lys01-3-didrik2@cisco.com")));
         //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.python.com")));
