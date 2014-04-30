abaaa4d0f4c53af7ac15d9d114f57bd2cd3e54c4
Syver Enstad <syvensta@cisco.com>
2014-04-30 13:59:21 +0200

    Using organizer email to call sip

--- src/com/example/myapp/MyActivity.java
+++ src/com/example/myapp/MyActivity.java
@@ -25,14 +25,38 @@ class Meeting {
 }
 
 public class MyActivity extends ListActivity {
+    private void debugCalendars() {
+        Cursor calCursor = getContentResolver().query(
+                CalendarContract.Events.CONTENT_URI,   // The content URI of the words table
+                new String[] {
+                        CalendarContract.Events.DTSTART,
+                        CalendarContract.Events.DTEND,
+                        CalendarContract.Events.CALENDAR_DISPLAY_NAME,
+                        CalendarContract.Events.DESCRIPTION,
+                        CalendarContract.Events.ORGANIZER,
+                        CalendarContract.Events.TITLE
+                },
+                null,
+                null,
+                null);
+
+        if (calCursor.moveToFirst()) {
+            do {
+                System.out.println(calCursor.getString(0) + " " + calCursor.getString(1) + " " + calCursor.getString(2)
+                        + "  " + calCursor.getString(3) + " " + calCursor.getString(4) + " " + calCursor.getString(5));
+            } while (calCursor.moveToNext());
+        }
+
+
+    }
     private List<Meeting> getEventList() {
         GregorianCalendar beginWindow = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        beginWindow.setTime(new Date(Integer.MIN_VALUE));
-        beginWindow.roll(Calendar.DAY_OF_MONTH, false);
+        beginWindow.setTime(new Date());
+        beginWindow.roll(Calendar.DAY_OF_YEAR, false);
 
         GregorianCalendar endWindow = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        endWindow.setTime(new Date(Integer.MAX_VALUE));
-        endWindow.roll(Calendar.DAY_OF_MONTH, true);
+        endWindow.setTime(new Date());
+        endWindow.roll(Calendar.DAY_OF_YEAR, true);
 
         String[] projection =
                 new String[] {
@@ -47,6 +71,18 @@ public class MyActivity extends ListActivity {
                 projection,
                 beginWindow.getTimeInMillis(),
                 endWindow.getTimeInMillis());
+
+//        Uri.Builder builder = CalendarContract.Instances.CONTENT_URI.buildUpon();
+//        ContentUris.appendId(builder, beginWindow.getTimeInMillis());
+//        ContentUris.appendId(builder, endWindow.getTimeInMillis());
+//        Cursor calCursor = getContentResolver().query(
+//                builder.build(),
+//                projection,
+//                null, null,
+//                "begin ASC");
+
+
+
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy hh:mm");
         ArrayList<Meeting> result = new ArrayList<Meeting>();
         if (calCursor.moveToFirst()) {
@@ -58,23 +94,20 @@ public class MyActivity extends ListActivity {
                 end.setTimeInMillis(calCursor.getLong(2));
                 String title = calCursor.getString(4);
                 long eventId = calCursor.getLong(3);
-                Cursor attendeeCursor = CalendarContract.Attendees.query(getContentResolver(), eventId, new String[] {CalendarContract.Attendees.ATTENDEE_EMAIL, CalendarContract.Attendees.ATTENDEE_RELATIONSHIP});
-                String email = "";
+                Cursor attendeeCursor = CalendarContract.Attendees.query(getContentResolver(), eventId, new String[] {CalendarContract.Attendees.ATTENDEE_EMAIL, CalendarContract.Attendees.ATTENDEE_RELATIONSHIP, CalendarContract.Attendees.ORGANIZER});
+                String organizerEmail = "";
                 String sipAddress = "";
                 if (attendeeCursor.moveToFirst()) {
-                    do {
-                        if (attendeeCursor.getInt(1) == CalendarContract.Attendees.RELATIONSHIP_ORGANIZER) {
-                            email = attendeeCursor.getString(0);
-                            // split @ prefix + ".meet" + "@" + suffix
-                            String[] prefixAndSuffix = email.split("@");
-                            sipAddress = prefixAndSuffix[0] + ".meet@" + prefixAndSuffix[1];
-                        }
-                    } while (attendeeCursor.moveToNext());
+                    System.out.println("Attendee - EMAIL: " + attendeeCursor.getString(0) + " RELATIONSHIP: " + attendeeCursor.getString(1) + " ORGANIZER" + attendeeCursor.getString(2));
+                    organizerEmail = attendeeCursor.getString(2);
+                    // split @ prefix + ".meet" + "@" + suffix
+                    String[] prefixAndSuffix = organizerEmail.split("@");
+                    sipAddress = "sip:" + prefixAndSuffix[0] + ".meet@" + prefixAndSuffix[1];
                 }
                 Meeting meeting = new Meeting();
-                meeting.displayName = title + " " + formatter.format(begin.getTime()) + " " + formatter.format(end.getTime()) + " " + "organizer mail: " + email;
+                meeting.displayName = title + " " + formatter.format(begin.getTime()) + " " + formatter.format(end.getTime()) + " " + "organizer mail: " + organizerEmail;
                 meeting.sipAddress = sipAddress;
-                System.out.println("added " + sipAddress);
+                System.out.println("added " + sipAddress + " description: " + meeting);
                 result.add(meeting);
             } while (calCursor.moveToNext());
         }
@@ -84,8 +117,10 @@ public class MyActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //debugCalendars();
+        System.out.println("before event list");
         List<Meeting> events = getEventList();
-        System.out.println("got here");
+
         setListAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, events));
 //        Intent intent = getIntent();
 //        if (intent.getAction() == Intent.ACTION_VIEW) {
@@ -98,6 +133,8 @@ public class MyActivity extends ListActivity {
     @Override
     public void onListItemClick(ListView list, View v, int position, long id) {
         super.onListItemClick(list, v, position, id);
-        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("sip:bnordlun.meet@cisco.com")));
+        Meeting meeting = (Meeting)list.getAdapter().getItem(position);
+        System.out.println("About to call: " + meeting.sipAddress);
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(meeting.sipAddress)));
     }
 }
